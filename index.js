ba6a64068840740c4f8678fa7920b45d12e8eff1
tkolsrud <mfliltim@gmail.com>
2021-11-22 11:32:44 -0800

    remade from scratch to address migration issue

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const methodOverride = require('method-override')
+
+const app = expressS()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('static'))
+app.use(methodOverride('_method'))
+
+// WRITE YOUR ROUTES HERE /////////////
+
+
+// YOUR ROUTES ABOVE THIS COMMENT /////
+
+app.listen(3000)
