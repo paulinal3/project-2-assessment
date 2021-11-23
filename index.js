4cb74be351be199a168b1197e79aefb315a87b5f
paulinal3 <paulinal3@outlook.com>
2021-11-23 09:47:36 -0500

    step 3.2 complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const methodOverride = require('method-override')
-
 const app = express()
+const db = require('./models')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
@@ -10,9 +10,20 @@ app.use(methodOverride('_method'))
 
 // WRITE YOUR ROUTES HERE /////////////
 
+// home route
 app.get('/', (req, res) => {
-    res.render('index')
+    db.widget.findAll()
+    .then(foundWidgets => {
+        console.log('these are the found widgets\n', foundWidgets)
+        res.render('index', {widgets: foundWidgets})
+    })
 })
+
+// GET/INDEX ROUTE TO DISPLAY ALL WIDGETS
+
+// GET/NEW ROUTE TO SHOW FORM TO ADD A NEW WIDGET
+
+// DELETE ROUTE TO DELETE A WIDGET
 
 // YOUR ROUTES ABOVE THIS COMMENT /////
 
